a3435a9b9d38f6a7b2a99bb5f2f416c4edc47bbc
Filip <s8145@pjwstk.edu.pl>
2013-11-03 18:17:36 +0100

    Update skrypt.js

--- RasPi/skrypt.js
+++ RasPi/skrypt.js
@@ -4,225 +4,248 @@ $(document).ready(function () {
     'use strict';
     var socket = io.connect();
 
-	var rozmiar = 700;
-	var robotX = 301, robotY = 301;
-	var robotHeadX = 301+20, robotHeadY = 301-7;
-	var paper = Raphael(mapa, rozmiar, rozmiar);
-function draw(){	
-	var liniePoziom = paper.path("M 0 0 l "+ rozmiar +" 0 ");
-	var liniePion = paper.path("M 0 0 l 0 "+ rozmiar);  
-	for(var i = 0; i < 1000; i+=20) {
-		liniePoziom = paper.path("M 0 "+i+" l "+rozmiar+" 0 ");
-		liniePion = paper.path("M"+i+" 0 l 0 "+rozmiar);  
-		liniePoziom.attr({stroke: 'white', 'stroke-width': 1});
-		liniePion.attr({stroke: 'white', 'stroke-width': 1});		
-	}
-	var robot = paper.path("M "+ robotX +" "+ robotY +" l 0 77 l 77 0 l 0 -77 z"); 
-	robot.attr({fill: '#B30000', stroke: 'green', 'stroke-width': 3}); 
+        var rozmiar = 700;
+        var robotX = 301, robotY = 301;
+        var robotHeadX = robotX+10, robotHeadY = robotY-3;
 
-	var robotHead = paper.path("M "+ robotHeadX+" "+ robotHeadY +"  l 10 0 l 0 -7 l 0 7 l 20 0 l 0 -7 l 0 7 l 10 0");
-	robotHead.attr({stroke: 'green', 'stroke-width': 7});  
+        var robotLIRX = robotX-3, robotLIRY = robotY+27;
+        var robotRIRX = robotX+41, robotRIRY = robotY+12;
+        var robotHIRX = robotX+13, robotHIRY = robotY-3;
+        
+
+
+
+var paper = Raphael(mapa, rozmiar, rozmiar);
+
+
+
+
+function draw(){        
+        var liniePoziom = paper.path("M 0 0 l "+ rozmiar +" 0 ");
+        var liniePion = paper.path("M 0 0 l 0 "+ rozmiar);  
+        for(var i = 0; i < 1000; i+=20) {
+                liniePoziom = paper.path("M 0 "+i+" l "+rozmiar+" 0 ");
+                liniePion = paper.path("M"+i+" 0 l 0 "+rozmiar);  
+                liniePoziom.attr({stroke: 'white', 'stroke-width': 1});
+                liniePion.attr({stroke: 'white', 'stroke-width': 1});                
+        }
+        var robot = paper.path("M "+ robotX +" "+ robotY +" l 0 38 l 38 0 l 0 -38 z"); 
+        robot.attr({fill: '#B30000', stroke: 'green', 'stroke-width': 2}); 
+
+        //var robotHead = paper.path("M "+ robotHeadX+" "+ robotHeadY +"  l 5 0 l 0 -3.5 l 0 3.5 l 10 0 l 0 -3.5 l 0 3.5 l 5 0");
+        //robotHead.attr({stroke: 'green', 'stroke-width': 3});
+        
+        var robotHIR = paper.path("M "+ robotHIRX+" "+ robotHIRY +   "  l 4 0 l 0 -3.5 l 0 3.5 l 6 0 l 0 -3.5 l 0 3.5 l 4 0");
+        robotHIR.attr({stroke: 'green', 'stroke-width': 3});
+
+        var robotLIR = paper.path("M "+ robotLIRX+" "+ robotLIRY+"l 0 -4 l -3.5 0 l  3.5 0 l 0 -6 l -3.5 0 l 3.5 0 l 0 -4");
+        robotLIR.attr({stroke: 'green', 'stroke-width': 3});
+        
+        var robotRIR = paper.path("M "+ robotRIRX+" "+ robotRIRY+"l 0 4 l 3.5 0 l  -3.5 0 l 0 6 l 3.5 0 l -3.5 0 l 0 4");
+        robotRIR.attr({stroke: 'green', 'stroke-width': 3});        
 }
 
 
-//--------------------------------------------------------------- obs³uga odpowiedzi serwera
+//--------------------------------------------------------------- obsÂ³uga odpowiedzi serwera
     socket.on('testServer', function( dane ){
         console.log( dane );
-		$("#d1").css( { "background-color": "green" } );
+                $("#d1").css( { "background-color": "green" } );
     });
-	
-	socket.on('testJSON', function( dane ){
+        
+        socket.on('testJSON', function( dane ){
         console.log( JSON.parse( dane ) );
-		$("#d2").css( { "background-color": "green" } );
+                $("#d2").css( { "background-color": "green" } );
     });
-	
-	socket.on('testRobot', function( dane ) {
-		
-		console.log('from skrypt'+ dane );
-		if( dane== 1 ) {
-			$("#d3").css( { "background-color": "green" } );
-		}
-		else {
-			$("#d3").css( { "background-color": "red" } );
-		}
-		paper.clear();	
-		var circle = paper.circle(robotHeadX+40, robotHeadY-dane, 8);
-		circle.attr("fill", "blue");
-		circle.attr("stroke", "#fff");
-		draw();
-	});
+        
+        socket.on('testRobot', function( dane ) {
+                
+                console.log('from skrypt'+ dane );
+                if( dane== 1 ) {
+                        $("#d3").css( { "background-color": "green" } );
+                }
+                else {
+                        $("#d3").css( { "background-color": "red" } );
+                }
+                $("#dane").html('<p>servo_1 detect: '+ dane + 'cm</p>');
+                paper.clear();        
+                var circle = paper.circle(robotHeadX+40, robotHeadY-dane*3, 8);
+                circle.attr("fill", "blue");
+                circle.attr("stroke", "#fff");
+                draw();
+                
+        });
 
-	
+        
 //------------------------------------------------------------------
 
 
 
 //------------------------------------------------------------------- Mysz
-	
-	$("#w").on( "mousedown", function() {
-		$(this).css( { "background-color": "blue" } );
-		socket.emit('jazda', 'w');
-		console.log('wcisniete W');
+        
+                $("#w").on( "mousedown", function() {
+                $(this).css( { "background-color": "blue" } );
+                socket.emit('jazda', 'w');
+                console.log('wcisniete W');
      })
-	 .on( "mouseup", function() {
-		$(this).css( { "background-color": "red" } );
-		socket.emit('jazda', '3');
-		console.log('niewcisniete W');
-	});
-	
-	$("#a").on( "mousedown", function() {
-		$(this).css( { "background-color": "blue" } );
-        	socket.emit('jazda', 'a');
-		console.log('wcisniete A');
+         .on( "mouseup", function() {
+                $(this).css( { "background-color": "red" } );
+                socket.emit('jazda', '3');
+                console.log('niewcisniete W');
+        });
+        
+        $("#a").on( "mousedown", function() {
+                $(this).css( { "background-color": "blue" } );
+                socket.emit('jazda', 'a');
+                console.log('wcisniete A');
      })
-	 .on( "mouseup", function() {
-		$(this).css( { "background-color": "red" } );
-		socket.emit('jazda', '3');
-		console.log('niewcisniete A');
-	});//robotHead.rotate(50,robotHeadX+20,robotHeadY);
-	
-	$("#s").on( "mousedown", function() {
-		$(this).css( { "background-color": "blue" } );
-        	socket.emit('jazda', 's');
-		console.log('wcisniete S');
+         .on( "mouseup", function() {
+                $(this).css( { "background-color": "red" } );
+                socket.emit('jazda', '3');
+                console.log('niewcisniete A');
+        });//robotHead.rotate(50,robotHeadX+20,robotHeadY);
+        
+        $("#s").on( "mousedown", function() {
+                $(this).css( { "background-color": "blue" } );
+                socket.emit('jazda', 's');
+                console.log('wcisniete S');
      })
-	 .on( "mouseup", function() {
-		$(this).css( { "background-color": "red" } );
-		socket.emit('jazda', '3');
-		console.log('niewcisniete S');
-	});
-	
-	$("#d").on( "mousedown", function() {
-		$(this).css( { "background-color": "blue" } );
-        	socket.emit('jazda', 'd');
-		console.log('wcisniete D');
+         .on( "mouseup", function() {
+                $(this).css( { "background-color": "red" } );
+                socket.emit('jazda', '3');
+                console.log('niewcisniete S');
+        });
+        
+        $("#d").on( "mousedown", function() {
+                $(this).css( { "background-color": "blue" } );
+                socket.emit('jazda', 'd');
+                console.log('wcisniete D');
      })
-	 .on( "mouseup", function() {
-		$(this).css( { "background-color": "red" } );
-		socket.emit('jazda', '3');
-		console.log('niewcisniete D');
-	});
-	$("#q").on( "mousedown", function() {
-		$(this).css( { "background-color": "blue" } );
-		socket.emit('jazda', 'q');
-        	robotHead.rotate(-5,robotHeadX+20,robotHeadY);
-		console.log('wcisniete Q');
+         .on( "mouseup", function() {
+                $(this).css( { "background-color": "red" } );
+                socket.emit('jazda', '3');
+                console.log('niewcisniete D');
+        });
+        $("#q").on( "mousedown", function() {
+                $(this).css( { "background-color": "blue" } );
+                socket.emit('jazda', 'q');
+                robotHead.rotate(-5,robotHeadX+20,robotHeadY);
+                console.log('wcisniete Q');
      })
-	 .on( "mouseup", function() {
-		$(this).css( { "background-color": "green" } );
-		socket.emit('jazda', '4');
-		console.log('niewcisniete Q');
-	});
-	$("#e").on( "mousedown", function() {
-		$(this).css( { "background-color": "blue" } );
-		socket.emit('jazda', 'e');
-        	robotHead.rotate(5,robotHeadX+20,robotHeadY);
-		console.log('wcisniete E');
+         .on( "mouseup", function() {
+                $(this).css( { "background-color": "green" } );
+                socket.emit('jazda', '4');
+                console.log('niewcisniete Q');
+        });
+        $("#e").on( "mousedown", function() {
+                $(this).css( { "background-color": "blue" } );
+                socket.emit('jazda', 'e');
+                robotHead.rotate(5,robotHeadX+20,robotHeadY);
+                console.log('wcisniete E');
      })
-	 .on( "mouseup", function() {
-		$(this).css( { "background-color": "green" } );
-		socket.emit('jazda', '4');
-		console.log('niewcisniete R');
-	});
-	
+         .on( "mouseup", function() {
+                $(this).css( { "background-color": "green" } );
+                socket.emit('jazda', '4');
+                console.log('niewcisniete R');
+        });
+        
 
 
-	$("#tryb1").on( "mousedown", function() {
-		$(this).css( { "background-color": "orange" } );
-		$("#tryb2").css( { "background-color": "yellow" } );
-		socket.emit('jazda', '0');
-		console.log('tryb servo');
+       $("#tryb1").on( "mousedown", function() {
+                $(this).css( { "background-color": "orange" } );
+                $("#tryb2").css( { "background-color": "yellow" } );
+                socket.emit('jazda', '0');
+                console.log('tryb servo');
       })
-	$("#tryb2").on( "mousedown", function() {
-		$(this).css( { "background-color": "orange" } );
-		$("#tryb1").css( { "background-color": "yellow" } );
-		socket.emit('jazda', '1');
-		console.log('tryb sonic');
+        $("#tryb2").on( "mousedown", function() {
+                $(this).css( { "background-color": "orange" } );
+                $("#tryb1").css( { "background-color": "yellow" } );
+                socket.emit('jazda', '1');
+                console.log('tryb sonic');
      })
-	 $("#skan").on( "mousedown", function() {
-		$(this).css( { "background-color": "yellow" } );
-		socket.emit('jazda', '5');
-		console.log('Skan manualny');
+         $("#skan").on( "mousedown", function() {
+                $(this).css( { "background-color": "yellow" } );
+                socket.emit('jazda', '5');
+                console.log('Skan manualny');
      })
-	 .on( "mouseup", function() {
-		$(this).css( { "background-color": "teal" } );
-	});
+         .on( "mouseup", function() {
+                $(this).css( { "background-color": "teal" } );
+        });
 //-------------------------------------------------------- Klawiatura
-	
-	$(document).on( "keydown", function(event) {
-		switch(event.keyCode) {
-		case 87:
-			$("#w").css( { "background-color": "blue" } );
-			socket.emit('jazda', 'w');
-			console.log('wcisniete klawisz W');
-			break;
-		case 65:
-			$("#a").css( { "background-color": "blue" } );
-			socket.emit('jazda', 'a');
-			console.log('wcisniete klawisz A');
-			break;
-		case 83:
-			$("#s").css( { "background-color": "blue" } );
-			socket.emit('jazda', 's');
-			console.log('wcisniete klawisz S');
-			break;
-		case 68:
-			$("#d").css( { "background-color": "blue" } );
-			socket.emit('jazda', 'd');
-			console.log('wcisniete klawisz D');
-			break;
+        
+   $(document).on( "keydown", function(event) {
+                switch(event.keyCode) {
+                case 87:
+                        $("#w").css( { "background-color": "blue" } );
+                        socket.emit('jazda', 'w');
+                        console.log('wcisniete klawisz W');
+                        break;
+                case 65:
+                        $("#a").css( { "background-color": "blue" } );
+                        socket.emit('jazda', 'a');
+                        console.log('wcisniete klawisz A');
+                        break;
+                case 83:
+                        $("#s").css( { "background-color": "blue" } );
+                        socket.emit('jazda', 's');
+                        console.log('wcisniete klawisz S');
+                        break;
+                case 68:
+                        $("#d").css( { "background-color": "blue" } );
+                        socket.emit('jazda', 'd');
+                        console.log('wcisniete klawisz D');
+                        break;
 
-		case 81:
-			$("#q").css( { "background-color": "blue" } );
-			socket.emit('jazda', 'q');
-        		robotHead.rotate(-5,robotHeadX+20,robotHeadY);
-			console.log('wcisniete Q');
-			break;
-		case 69:
-			$("#e").css( { "background-color": "blue" } );
-			socket.emit('jazda', 'e');
-        		robotHead.rotate(5,robotHeadX+20,robotHeadY);
-			console.log('wcisniete E');
-			break;
-		}
-		
+                case 81:
+                        $("#q").css( { "background-color": "blue" } );
+                        socket.emit('jazda', 'q');
+                        robotHead.rotate(-5,robotHeadX+20,robotHeadY);
+                        console.log('wcisniete Q');
+                        break;
+                case 69:
+                        $("#e").css( { "background-color": "blue" } );
+                        socket.emit('jazda', 'e');
+                        robotHead.rotate(5,robotHeadX+20,robotHeadY);
+                        console.log('wcisniete E');
+                        break;
+                }
+                
      })
-	 .on( "keyup", function(event) {
-		switch(event.keyCode) {
-		case 87:
-			$("#w").css( { "background-color": "red" } );
-			socket.emit('jazda', '3');
-			console.log('niewcisniete klawisz W');
-			break;
-		case 65:
-			$("#a").css( { "background-color": "red" } );
-			socket.emit('jazda', '3');
-			console.log('niewcisniete klawisz A');
-			break;
-		case 83:
-			$("#s").css( { "background-color": "red" } );
-			socket.emit('jazda', '3');
-			console.log('niewcisniete klawisz S');
-			break;
-		case 68:
-			$("#d").css( { "background-color": "red" } );
-			socket.emit('jazda', '3');
-			console.log('niewcisniete klawisz D');
-			break;
-		case 81:
-			$("#q").css( { "background-color": "green" } );
-			socket.emit('jazda', '4');
-			break;
-		
-		case 69:
-			$("#e").css( { "background-color": "green" } );
-			socket.emit('jazda', '4');
-			break;
+      .on( "keyup", function(event) {
+                switch(event.keyCode) {
+                case 87:
+                        $("#w").css( { "background-color": "red" } );
+                        socket.emit('jazda', '3');
+                        console.log('niewcisniete klawisz W');
+                        break;
+                case 65:
+                        $("#a").css( { "background-color": "red" } );
+                        socket.emit('jazda', '3');
+                        console.log('niewcisniete klawisz A');
+                        break;
+                case 83:
+                        $("#s").css( { "background-color": "red" } );
+                        socket.emit('jazda', '3');
+                        console.log('niewcisniete klawisz S');
+                        break;
+                case 68:
+                        $("#d").css( { "background-color": "red" } );
+                        socket.emit('jazda', '3');
+                        console.log('niewcisniete klawisz D');
+                        break;
+                case 81:
+                        $("#q").css( { "background-color": "green" } );
+                        socket.emit('jazda', '4');
+                        break;
+                
+                case 69:
+                        $("#e").css( { "background-color": "green" } );
+                        socket.emit('jazda', '4');
+                        break;
 
-		}
+                }
 
-	});
-	
+        });
+        
 //---------------------------------------------------------------------
-	
+        
 });
