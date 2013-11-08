a6f597972b0bea8f5d49cc5af7e521c85f850cc7
Maciej <s9202@pjwstk.edu.pl>
2013-11-08 16:06:36 +0100

    Komentarze do interfejsu

--- Arduino/funkcje.h
+++ Arduino/funkcje.h
@@ -4,6 +4,7 @@
 #include <Servo.h>
 #include "Arduino.h"
 
+//Znaki oczekiwane z portu szeregowego
 const char TRYB_MANUAL = '0';
 const char TRYB_AUTO = '1';
 const char JAZDA_PRZOD = 'w';
@@ -19,40 +20,42 @@ const char GLOWA_PRAWO = 'e';
 const char JAZDA_STOP = '3';
 const char GLOWA_STOP = '4';
 const char SKAN_MAN = '5';
-//const char JAZDA_STOP1 = '2';
-//const char JAZDA_STOP2 = '4';
-//const char JAZDA_STOP3 = '6';
-//const char JAZDA_STOP4 = '0';
-//const char GLOWA_STOP1 = 'a';
-//const char GLOWA_STOP2 = 'b';
 
-const double IR_MIN = 4.0;
-const double IR_MAX = 30.0;
-
+//Okreslenie rozmiaru boku mapy i wyliczenia mapy
 const int ROZMIAR_BOKU_MAPY = 10;
 const int ROZMIAR_MAPY = ROZMIAR_BOKU_MAPY * ROZMIAR_BOKU_MAPY;
 const int PG_ROG = ROZMIAR_BOKU_MAPY - 1;
 const int LD_ROG = ROZMIAR_MAPY - ROZMIAR_BOKU_MAPY;
 const int PD_ROG = ROZMIAR_MAPY - 1;
 
-//Biegunowa pozycja robota
+//Biegunowa pozycja robota widoczna na mapie
 const char PRZODEM_GORA = '8';
 const char PRZODEM_PRAWO = '6';
 const char PRZODEM_LEWO = '4';
 const char PRZODEM_DOL = '2';
 
+//Znaki wykorzystywane do wyswietlania mapy
 const char ZNAK_SCIANA = 'x';
 const char ZNAK_WOLNE = ' ';
 const char ZNAK_TRASA = '.';
 const char ZNAK_CEL = 'C';
 
+//Przyjete oznaczenie braku wezla
 const int BRAK_WEZLA = -1;
 
+//Wartosci okreslajace zasieg wykorzystywanych czujnikow
+const double IR_MIN = 4.0;
+const double IR_MAX = 30.0;
+
+//Piny odpowiadajace kazdemu czujnikowi
 const int IRLeft = 0;
 const int IRRight = 1;
 const int IRFront = 2;
 
+//Licznik okreslajacy dopuszczalna liczbe nieudanego znalezienia drogi do celu
+const int LICZBA_REZYGNACJI_CELU = 4;
 
+//Struktura wezla z jakich sklada sie mapa
 typedef struct {
 	int numerPoprzednika;
 	int sasiadZLewej;
@@ -62,6 +65,7 @@ typedef struct {
 	char rodzajWezla;
 } Wezel;
 
+//Struktura opisujaca pozycje i miejsce robota
 typedef struct Robot {
 	int miejsceRobota;
 	char pozycjaRobota;
@@ -74,22 +78,20 @@ int wykonajJedenRuchTyl(Wezel tablica[], int miejsceRobota, char pozycjaRobota, 
 char wykonajObrot90Lewo(Wezel tablica[], int miejsceRobota, char pozycjaRobota, Servo servo1, Servo servo2);
 char wykonajObrot90Prawo(Wezel tablica[], int miejsceRobota, char pozycjaRobota, Servo servo1, Servo servo2);
 
-//Funkcje ruchu cigego przydatne dla kontroli zdalnej
+//Funkcje ruchu ciglego przydatne dla kontroli zdalnej
 void jedzProsto(Servo servo1, Servo servo2);
 void skrecajWLewo(Servo servo1, Servo servo2);
 void skrecajWPrawo(Servo servo1, Servo servo2);
 void jedzDoTylu(Servo servo1, Servo servo2);
 void zatrzymajRuch(Servo servo1, Servo servo2);
 
-//Funkcje ruchu gowy
+//Funkcje ruchu glowy
 void zatrzymajGlowe(Servo servo3);
 void glowaLewo(Servo servo3);
 void glowaPrawo(Servo servo3);
 
-//Funkcje odczytu danych z czujnikw otoczenia (Filip)
-double skanujSensorPrzedni(int IRFront);
-double skanujSensorLewy(int IRLeft);
-double skanujSensorPrawy(int IRRight);
+//Funkcja odczytu danych z czujnikow otoczenia
+double skanujSensor(int iR);
 
 //Funkcje tworzce map (Maciej)
 int sprawdzOdlegloscIZaznacz(double odleglosc, int miejsceRobota, char pozycjaCzujnika, Wezel tablica[], int rozmiarTablicy, int rozmiarBoku);
