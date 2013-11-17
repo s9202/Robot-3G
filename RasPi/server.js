dffabb3b7a684a540afaa7e4375223af6f401779
Maciej <s9202@pjwstk.edu.pl>
2013-11-17 22:09:03 +0100

    mała zmiana serwer

--- RasPi/server.js
+++ RasPi/server.js
@@ -85,7 +85,7 @@ socket.on('connection', function( client ) {
                 
 //------------------------------------------------------------------         
                serialPort.on('data', function( dane ) {
-                           
+                        console.log("DANE:" +dane);   
                         var daneArduino = JSON.parse( dane );
 //                        console.log('Mapa odebrana od arduino: ' + daneArduino.mapa);
                         client.emit( 'testRobot', daneArduino);
